7c13650f51c1f9dbffda417bf5f60bf249e55f4c
Md.Nobin <mdnobin.fdr@gmail.com>
2025-11-24 20:23:10 +0600

    remove unnecessary server setup and export from app.js

--- src/app.js
+++ src/app.js
@@ -3,11 +3,3 @@
 
 const express = require('express');const app = express();
 const port = 3000;  
-app.get('/', (req, res) => {  
-  res.send('Hello World!');  
-});
-app.listen(port, () => {  
-  console.log(`Server is running at http://localhost:${port}`);  
-});
-module.exports = app;
-// export the app for testing or further usage
